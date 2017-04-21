f0f8530b626f4c6a20adbf8632642361366c65fb
joshua navarro ruiz <joshua.navarro35@gmail.com>
2017-04-21 18:26:29 -0500

    button Agregar added on createSection2

--- src/models/menu.js
+++ src/models/menu.js
@@ -3,17 +3,17 @@
 //     {
 //       id: 's1',
 //       sectionName: 'Lasagnas',
-//       products: [
-//         {
-//           id: 'p1',
-//           productName: 'Carnibora',
-//           productDescription: 'Queso, carne, tomate',
-//           urlImage: 'http://unsplash.it/123',
-//           price: 20,
-//         },
-//         {
-//           ... other product
-//         },
+      // products: [
+      //   {
+      //     id: 'p1',
+      //     productName: 'Carnibora',
+      //     productDescription: 'Queso, carne, tomate',
+      //     urlImage: 'http://unsplash.it/123',
+      //     price: 20,
+      //   },
+      //   {
+      //     ... other product
+      //   },
 //       ],
 //     },
 //     {
